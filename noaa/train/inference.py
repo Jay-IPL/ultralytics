1e9936e6791c98effef49e84b0c0fe89b076b5ae
Jay-IPL <meijie0401@gmail.com>
2023-12-05 00:22:20 -0800

    verbose false

--- noaa/train/inference.py
+++ noaa/train/inference.py
@@ -40,7 +40,7 @@ if __name__ == '__main__':
                           conf=args.confidence_threshold, # 0.167 is searched during detector training.
                           iou=0.7, # default
                           imgsz=640, # default, also is my trained model input size.
-                          verbose=True, # disable showing frames
+                          verbose=False, # disable showing frames
                           persist=True, 
                         #   epochs=50,
                         #   imgsz=640,
